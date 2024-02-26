d9c2a9cd1ade4bdf6608be844452258550e425f5
Samuel-murugan <samuellss3021@gmail.com>
2024-02-26 12:15:31 +0530

    Callback-Task

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    document.getElementById("display").innerText = " Countdown:10";
+    setTimeout(() => {
+      document.getElementById("display").innerText = "Countdown:9";
+      setTimeout(() => {
+        document.getElementById("display").innerText = "Countdown:8";
+        setTimeout(() => {
+          document.getElementById("display").innerText = "Countdown:7";
+          setTimeout(() => {
+            document.getElementById("display").innerText = "Countdown:6";
+            setTimeout(() => {
+              document.getElementById("display").innerText = "Countdown:5";
+              setTimeout(() => {
+                document.getElementById("display").innerText = "Countdown:4";
+                setTimeout(() => {
+                  document.getElementById("display").innerText = "Countdown:3";
+                  setTimeout(() => {
+                    document.getElementById("display").innerText = "Countdown:2";
+                    setTimeout(() => {
+                      document.getElementById("display").innerText = "Countdown:1";
+                      setTimeout(() => {
+                        document.getElementById("display").innerText =
+                          "Happy Independence day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
